602496e1f5ca3c2289952b185d7d62d9d29c90be
Eric Kleihege <aggies425@gmail.com>
2019-02-18 14:11:00 -0600

    remove classaverage model/wasn't implementing correctly

--- students/models.py
+++ students/models.py
@@ -4,7 +4,5 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=15)
     last_name = models.CharField(max_length=15)
-
-class ClassAverage(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    grade = models.IntegerField(default=0)
+    def __str__(self):
+        return self.first_name + " " + self.last_name
