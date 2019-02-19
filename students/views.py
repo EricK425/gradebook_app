0cc34c56ba0261ed3edd913c3f11ee8197d86d80
Eric Kleihege <aggies425@gmail.com>
2019-02-19 11:24:55 -0600

    add list functionalty to student index view

--- students/views.py
+++ students/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Student
+
 # Create your views here.
 def index(request):
-    return HttpResponse("All students:")
+    all_students = Student.objects.all()
+    output = ([student.first_name + ' ' + student.last_name + '\n' for student in all_students])
+    return HttpResponse(output)
 def detail(request, student_first_name, student_last_name):
     return HttpResponse("Student details for %s %s:" %(student_first_name, student_last_name))
 def update(request, student_first_name, student_last_name):
