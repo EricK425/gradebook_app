3a30801b151b9a82836f9fa207d5d6d8cc51e1a8
Eric Kleihege <aggies425@gmail.com>
2019-02-19 12:49:42 -0600

    add student templates dir and create index template

--- students/views.py
+++ students/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Student
 
 # Create your views here.
 def index(request):
-    all_students = Student.objects.all()
-    output = ([student.first_name + ' ' + student.last_name + '\n' for student in all_students])
-    return HttpResponse(output)
+    all_students = Student.objects.order_by('last_name')
+    template = loader.get_template('students/index.html')
+    context = {
+        'all_students': all_students,
+    }
+    return HttpResponse(template.render(context,request))
+
 def detail(request, student_first_name, student_last_name):
     return HttpResponse("Student details for %s %s:" %(student_first_name, student_last_name))
+
 def update(request, student_first_name, student_last_name):
     return HttpResponse("Update student details for %s %s:" %(student_first_name, student_last_name))
