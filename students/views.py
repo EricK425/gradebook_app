f6de66ec01f016ba6e613c358352212e4f379a71
Eric Kleihege <aggies425@gmail.com>
2019-02-19 14:08:51 -0600

    refactor views and index template for students

--- students/views.py
+++ students/views.py
@@ -7,11 +7,10 @@ from .models import Student
 # Create your views here.
 def index(request):
     all_students = Student.objects.order_by('last_name')
-    template = loader.get_template('students/index.html')
     context = {
         'all_students': all_students,
     }
-    return HttpResponse(template.render(context,request))
+    return render(request, 'students/index.html', context)
 
 def detail(request, student_first_name, student_last_name):
     return HttpResponse("Student details for %s %s:" %(student_first_name.title(), student_last_name.title()))
